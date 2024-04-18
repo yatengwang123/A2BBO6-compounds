584dae475a010d87552b3cd985de76048b0d32bd
yatengwang123 <yatengwang123@163.com>
2024-04-18 10:20:19 +0800

    update

--- ML_code/data_spilt.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-df=pd.read_excel('feature_data_set.xlsx')
-# print(df.shape)
-train,val=train_test_split(df,train_size=0.85)
-train.to_csv('train_data_set.csv',index=False)
-val.to_csv('val_data_set.csv',index=False)
